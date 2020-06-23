5f8b951bb41915abbdcd7d48274df46d950bea92
Jeremy CINELLI <jeremy.cinelli@gmail.com>
2020-06-23 15:08:00 +0200

    1er commit / Approche imperative

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,13 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "Hello !";
+		
+		System.out.println(str);
+	}
+
+}
